981b1e94d9a7f405cc46e2e6b2c82636fe1679c1
Guilherme Maruta <168042965+hiyuk1@users.noreply.github.com>
2025-07-24 07:07:38 +0000

    chore: remove test files
    - Removed codigo_do_pai.js, codigo_do_pai.py, codigo_hardcore.py, hello.py
    - These were temporary test files that should not be in the repository

--- codigo_do_pai.js
+++
@@ -1,7 +0,0 @@
-print("Hello world")cd '/workspaces/my-portfolio-site'
-cd '/workspaces/my-portfolio-site'
-cd '/workspaces/my-portfolio-site'
-cd '/workspaces/my-portfolio-site'
-print("Hello world")
-cd '/workspaces/my-portfolio-site'
-
--- codigo_hardcore.py
+++
@@ -1,2 +0,0 @@
-print("Hello world!")
-## vai se foder copilot, eu fiquei tentando fazer tipo ^G, ctrl + G pra conseguir ajuda mas ele ficava abrindo aba do codespace, por isso saiu aquele tanto de cd cd cd
